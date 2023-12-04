37d378c73fd4ea3bee0715fbca4d840f9d20487e
PinoGabriel <pinogabriel01@gmail.com>
2023-12-04 15:43:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import axios from 'axios'
+import { store } from './store.js'
+export default {
+  components: {
+    AppHeader
+  },
+  data () {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <h1>Ciao</h1>
+      <div>
+        <input type="text" placeholder="cerca il tuo film preferito">
+        <button>Cerca</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background-color: #141414;
+}
+
+.container {
+  width: 1100px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 15vh;
+}
+
+h1 {
+  color: red;
+}
+
+input {
+  margin-right: 8px;
+}
+</style>
