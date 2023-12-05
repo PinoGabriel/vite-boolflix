cbed4a590453f7bdffb94669bce789d38a07612b
PinoGabriel <pinogabriel01@gmail.com>
2023-12-05 11:25:59 +0100

    feat: added $emit

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,42 @@ import AppCard from './components/AppCard.vue'
 import axios from 'axios'
 import { store } from './store.js'
 export default {
-  components: {
-    AppHeader,
-    AppCard
-  },
-  data () {
-    return {
-      store,
-    }
-  },
-  mounted() {
-      
+    components: {
+        AppHeader,
+        AppCard
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        pushText() {
+            axios.get(this.store.APIUrlMovie + "&query=" + encodeURIComponent(this.store.searchText)).then(result => {
+                this.store.film = result.data.results
+                console.log(result.data);
+                console.log(this.store.film);
+                console.log(this.store.searchText);
+            })
+            axios.get(this.store.APIUrlSeries + "&query=" + encodeURIComponent(this.store.searchText)).then(result => {
+                this.store.series = result.data.results
+                console.log(result.data.results);
+                console.log(this.store.series);
+                console.log(this.store.searchText);
+            })
+        },
+    },
+    mounted() {
+
     }
 }
 </script>
 
 <template>
-  <AppHeader/>
-  <main>
-    <AppCard/>
-  </main>
+    <AppHeader @search="pushText()" />
+    <main>
+        <AppCard />
+    </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,63 +2,53 @@
 import { store } from '../store.js'
 import axios from 'axios'
 export default {
-  name: "AppHeader",
-  data () {
-    return {
-      store
+    name: "AppHeader",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        search() {
+            this.$emit('search')
+        }
     }
-  },
-  methods: {
-    pushText() {
-      axios.get(this.store.APIUrlMovie + "&query=" + encodeURIComponent(this.store.searchText)).then(result =>{
-        this.store.film = result.data.results
-        console.log(result.data);
-        console.log(this.store.film);
-        console.log(this.store.searchText);
-      })
-      axios.get(this.store.APIUrlSeries + "&query=" + encodeURIComponent(this.store.searchText)).then(result =>{
-        this.store.series = result.data.results
-        console.log(result.data.results);
-        console.log(this.store.series);
-        console.log(this.store.searchText);
-      })
-    }
-  }
 }
 </script>
 
 <template>
-  <section>
-    <div class="container">
-      <h1>BoolFlix</h1>
-      <div>
-        <input type="text" v-model="store.searchText" placeholder="cerca il tuo film preferito">
-        <button @click="pushText()">Cerca</button>
-      </div>
-    </div>
-  </section>
+    <section>
+        <div class="container">
+            <h1>BoolFlix</h1>
+            <div>
+                <input type="text" v-model="store.searchText" placeholder="cerca il tuo film preferito"
+                    @keyup.enter="search()">
+                <button @click="search()">Cerca</button>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped>
 section {
-  background-color: #141414;
+    background-color: #141414;
 }
 
 .container {
-  width: 1100px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  min-height: 15vh;
+    width: 1100px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 15vh;
 }
 
 h1 {
-  color: red;
-  font-size: 2rem;
+    color: red;
+    font-size: 2rem;
 }
 
 input {
-  margin-right: 8px;
+    margin-right: 8px;
 }
 </style>
